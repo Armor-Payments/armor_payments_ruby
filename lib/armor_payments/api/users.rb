9bca74c843a62961a24f1a9834b4eaaaa0dbd8b9
Mark French <mrmarkfrench@gmail.com>
2014-09-30 14:11:29 -0400

    Adding update method to Users resource.

--- lib/armor_payments/api/users.rb
+++ lib/armor_payments/api/users.rb
@@ -1,5 +1,10 @@
 module ArmorPayments
   class Users < Resource
 
+    def update account_id, data
+      headers = authenticator.secure_headers 'post', uri(account_id)
+      request :post, { path: uri(account_id), headers: headers, body: JSON.generate(data) }
+    end
+
   end
 end
