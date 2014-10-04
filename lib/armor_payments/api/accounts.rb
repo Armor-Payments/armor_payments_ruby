d083c1a413b30038cf08df04bf60eb97372e3abe
Mark French <mrmarkfrench@gmail.com>
2014-10-04 16:58:24 -0400

    Fix to arguments for accounts.update

--- lib/armor_payments/api/accounts.rb
+++ lib/armor_payments/api/accounts.rb
@@ -6,9 +6,9 @@ module ArmorPayments
       request :post, { path: uri, headers: headers, body: JSON.generate(data) }
     end
     
-    def update data
-      headers = authenticator.secure_headers 'post', uri
-      request :post, { path: uri, headers: headers, body: JSON.generate(data) }
+    def update account_id, data
+      headers = authenticator.secure_headers 'post', uri(account_id)
+      request :post, { path: uri(account_id), headers: headers, body: JSON.generate(data) }
     end
 
   end
