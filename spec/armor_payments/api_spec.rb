e4918f05939e3df599035ac4f06b6f19111edd4c
Brian Kung <brian@callmekung.com>
2015-05-07 15:39:03 -0500

    Fixes api_spec, prefers new expect syntax

--- spec/armor_payments/api_spec.rb
+++ spec/armor_payments/api_spec.rb
@@ -7,15 +7,15 @@ module ArmorPayments
     describe "#armor_host" do
       context "in sandbox mode" do
         it "returns https://sandbox.armorpayments.com" do
-          client.sandbox.should be_true
-          client.armor_host.should == "https://sandbox.armorpayments.com"
+          expect(client.sandbox).to be true
+          expect(client.armor_host).to eq "https://sandbox.armorpayments.com"
         end
       end
 
       context "*not* in sandbox mode" do
         it "returns https://api.armorpayments.com" do
           client.sandbox = false
-          client.armor_host.should == "https://api.armorpayments.com"
+          expect(client.armor_host).to eq "https://api.armorpayments.com"
         end
       end
     end
