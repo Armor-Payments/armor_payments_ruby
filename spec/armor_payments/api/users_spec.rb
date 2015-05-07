7e2e650e11fd062af45f0f563f4cb819aba5b699
Brian Kung <brian@callmekung.com>
2015-05-07 17:19:35 -0500

    State exact path to users in spec

--- spec/armor_payments/api/users_spec.rb
+++ spec/armor_payments/api/users_spec.rb
@@ -7,11 +7,11 @@ module ArmorPayments
     let(:users) { Users.new(host, authenticator, '/accounts/1234') }
 
     describe "#uri" do
-      it "returns '/users' if given no id" do
+      it "returns 'accounts/:aid/users' if given no id" do
         expect(users.uri).to eq '/accounts/1234/users'
       end
 
-      it "returns '/users/:id' if given an id" do
+      it "returns 'accounts/:aid/users/:id' if given an id" do
         expect(users.uri(456)).to eq '/accounts/1234/users/456'
       end
     end
